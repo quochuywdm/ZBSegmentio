9ac5e8e008b3541a6af647000f2b080d18c121fa
Quoc Huy Nguyen <45702175+quochuywdm@users.noreply.github.com>
2023-07-10 12:57:51 +0200

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Segmentio",
+    platforms: [.iOS(.v13)],
     products: [
         .library(name: "Segmentio", targets: ["Segmentio"]),
     ],
